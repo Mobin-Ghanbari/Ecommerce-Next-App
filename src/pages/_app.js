6f96e18dd8c66652d6620729380b4ac0cd7e1be0
mobin.gh <mobinghanbari.512@gmail.com>
2023-04-23 13:53:58 +0430

    start single-product page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,23 @@
-import '@/styles/globals.css'
+import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/header";
+import Head from "next/head";
+
+import "@/styles/globals.css";
+import DiscountBanner from "@/components/discount-banner/DiscountBanner";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="w-full overflow-x-hidden">
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        ></link>
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+      <DiscountBanner />
+      <Footer />
+    </div>
+  );
 }
